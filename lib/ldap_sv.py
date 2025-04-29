00d8d6c10a17cb3996e915dc57ded10a47da715b
Ivanciu Andrei <47902631+hawkhero2@users.noreply.github.com>
2025-04-29 16:22:20 +0300

    wip ldap class

--- lib/ldap_sv.py
+++ lib/ldap_sv.py
@@ -1,4 +1,3 @@
-from dotenv import dotenv_values
 from paramiko import SSHClient
 from lib.globals_vars import LOGFILE, LOGS_FORMAT, ENV
 import logging
@@ -6,22 +5,36 @@ import logging
 
 class LdapServer():
     """
-    
+    Class which manages the users on a LDAP
+    Currently supports the following functionalities : 
     """
 
     def __init__(self):
         self.env = ENV()
+        self.ssh = None
         self.logger = logging.getLogger(__name__)
         logging.basicConfig(filename=LOGFILE, format=LOGS_FORMAT, level=logging.INFO)
 
-    def disconnect():
-        pass
+    def disconnect(self):
+        """Closes the connection with the target host"""
+        if self.ssh:
+            self.ssh.close()
 
-    def connect():
-        pass
+    def connect(self):
+        """Connects to the target host machine through SSH"""
+        if self.ssh != None:
+            self.ssh = SSHClient()
+            self.ssh.load_system_host_keys()
 
     def update_user_info():
         pass
 
     def get_user_info():
         pass
+
+    def __enter__(self):
+        self.connect()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.disconnect()
